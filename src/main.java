a934ab2d52299bf4636961204cb1641dca26bb5d
Alex Nagy <alexnagy@spacefiller.space>
2022-02-03 11:09:18 -0800

    First Commit

---
+++ src/main.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class main extends PApplet {
+
+    public static void main(String... args){
+        PApplet.main("main");
+    }
+
+    public void settings(){
+        size(200, 200);
+    }
+
+    public void setup(){
+        println("HELLO WORLD");
+    }
+
+    public void draw(){
+       //
+    }
+}
